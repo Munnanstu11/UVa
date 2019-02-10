fed20f4a547a7a496d32806bb416d6a2efc3c3d1
Md Monir Uz Zaman Munna <munna.nstu16@gmail.com>
2019-02-11 02:13:42 +0600

    Add files via upload

---
+++ uva11332.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    long long num,sum,rem;
+    while(scanf("%lld",&num) && num!=0)
+    {
+        while(1)
+        {
+            sum=0;
+            while(num!=0)
+            {
+                rem=num%10;
+                sum+=rem;
+                num=num/10;
+            }
+            num=sum;
+            if(sum<10)
+            {
+                break;
+            }
+
+
+        }
+        printf("%lld\n",sum);
+    }
+    return 0;
+}
